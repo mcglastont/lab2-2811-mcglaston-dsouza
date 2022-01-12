29ec43b0c0e2f23e288de668b4ba6c0c1d54bdcb
mcglastont <tjmcglaston@gmail.com>
2022-01-11 18:15:33 -0600

    Got the fxml for the garden to show up and began work on generation of bees and flows on the garden.

--- src/Garden.java
+++ src/Garden.java
@@ -1,3 +1,12 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -9,11 +18,18 @@ public class Garden {
     //variables
     private List<Bee> listOfBees;
     private List<Flower> listOfFlowers;
+    private List<ProgressBar> lifeForce;
 
-    //methods
-    public Garden (List<Bee> listOfBees, List<Flower> listOfFlowers) {
-        this.listOfBees = listOfBees;
-        this.listOfFlowers = listOfFlowers;
+    @FXML
+    private Pane theGarden;                 // capture the pane we are drawing on from JavaFX
+
+    @FXML
+    public void initialize() {              // executed after scene is loaded but before any methods
+        theGarden.setStyle("-fx-background-color: linear-gradient(to bottom right, derive(forestgreen, 20%), derive(forestgreen, -40%));");
+
+        //Start by generating the bees and randomly placing them in the garden
+
+        theGarden.setFocusTraversable(true); // ensure garden pane will receive keypresses
     }
 
     public void update() {
